5ce426095d37f123013268de533052d21caf9baf
Nick Hudkins <nick@nickhudkins.com>
2016-06-15 15:22:48 -0400

    add batch support

--- graphene/contrib/django/views.py
+++ graphene/contrib/django/views.py
@@ -1,4 +1,5 @@
 from graphql_django_view import GraphQLView as BaseGraphQLView
+from graphql_django_view import BatchGraphQLView as BatchBaseGraphQLView
 
 
 class GraphQLView(BaseGraphQLView):
@@ -6,6 +7,18 @@ class GraphQLView(BaseGraphQLView):
 
     def __init__(self, schema, **kwargs):
         super(GraphQLView, self).__init__(
+            graphene_schema=schema,
+            schema=schema.schema,
+            executor=schema.executor,
+            **kwargs
+        )
+
+
+class BatchGraphQLView(BatchBaseGraphQLView):
+    graphene_schema = None
+
+    def __init__(self, schema, **kwargs):
+        super(BatchGraphQLView, self).__init__(
             graphene_schema=schema,
             schema=schema.schema,
             executor=schema.executor,
